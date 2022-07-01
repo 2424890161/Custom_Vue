e7065f17b9a87b3a0bb1d08dccd88ed6d2c12428
caocaitao <2424890161@qq.com>
2022-07-01 10:17:22 +0800

    新增聊天室and登录页面

---
+++ src/components/ChatRoom.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChatRoom.vue",
+  data: function () {
+    return {
+      ip: "",
+      name: "",
+    };
+  },
+  methods: {
+
+  },
+  window: onload = function () {
+    this.dialogFormVisible = true;
+    let ip = sessionStorage.getItem("ip");
+    let name = sessionStorage.getItem("name");
+    this.ip = ip;
+    this.name = name + "-" + this.form.name;
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import ChatRoom from "@/components/ChatRoom";
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'ChatRoom',
+      component: ChatRoom
+    },
+    {
+      path: '/ChatRoom',
+      name: 'ChatRoom',
+      component: ChatRoom
     }
   ]
 })
